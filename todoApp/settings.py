169f67f7581ae2fc7ca4d1222e0bf120e8307720
高山怜也 <reiya@takayamareinarinoMacBook-Air.local>
2024-09-15 16:06:01 +0900

    9/15

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -93,8 +93,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Whitenoise settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
